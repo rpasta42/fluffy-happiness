f55b781d182c08d630581a5ad7acdc8ed0362606
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-08-11 09:08:02 +0000

    Working on main menu and switching tabs.

---
+++ frontpagedata/index.js
@@ -0,0 +1,40 @@
+$(function() {
+   var slideSpeed = 'fast'; //'slow';
+   var prevPage = '#page-home';
+
+   //$('.page').css('display', 'none');
+   $('.page').hide();
+   $(prevPage).show(); //slideDown();
+
+   /*var pos = 90 + $('#tux').width() + $('#name-text').width();
+   $('#top-tabs').css('left', pos + 'px');*/
+
+   $('.nav-i > a').hover(function () {
+      $(this).addClass('hovered');
+   });
+   $('.nav-i > a').mouseout(function() {
+      $(this).removeClass('hovered');
+   });
+
+   $('.nav-i').click(function () {
+      /*$('.nav-i').each(function() {
+         $(this).removeClass('active');
+      });*/
+      $('.nav-i').removeClass('active');
+      $(this).addClass('active');
+
+      var tabId = $(this).attr('id');
+      var pageId = '#page' + tabId.substr(3, tabId.length);
+
+      console.log('sliding: ' + pageId);
+      $(prevPage).slideUp(slideSpeed, function () {
+         $(pageId).slideDown(slideSpeed);
+      });
+
+      prevPage = pageId;
+
+
+   });
+
+});
+
